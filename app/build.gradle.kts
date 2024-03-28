d6c960add7ae58acac0b736ffe4ed4f0db3a4833
varunkumar2004 <varunkumar@gmail.com>
2024-03-28 20:02:39 +0530

    final changes before first push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.runtime:runtime:1.6.2")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.2")
+    implementation("androidx.compose.runtime:runtime:1.6.4")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.4")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
@@ -76,9 +76,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.material:material:1.6.2")
-    implementation("androidx.compose.material:material-icons-extended-android:1.6.2")
-    implementation("io.coil-kt:coil-compose:2.5.0")
+    implementation("androidx.compose.material:material:1.6.4")
+    implementation("androidx.compose.material:material-icons-extended-android:1.6.4")
+    implementation("io.coil-kt:coil-compose:2.6.0")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
